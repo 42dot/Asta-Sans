74f863da7bd9961d5f1edc692ba341a820c200e7
Aaron <aaron@sajatypeworks.com>
2024-12-20 05:57:27 -0800

    Removing prep table

--- sources/post.py
+++ sources/post.py
@@ -11,5 +11,10 @@ for file in Path("fonts").glob("**/*.ttf"):
 		font["name"].setName("42dot Sans",1,3,1,1033)
 		font["name"].setName("42dot Sans Regular",4,3,1,1033)
 		font["name"].setName("42dotSans-Regular",6,3,1,1033)
+	
+	try:
+		del font["prep"]
+	except KeyError:
+		pass
 
 	font.save(file)
